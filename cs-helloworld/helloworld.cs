035ecd352e12bec325e5976f6bd5698792104fe0
Alex5xt <86876406+Alex5xt@users.noreply.github.com>
2021-09-23 16:48:04 +0530

    hello world in csharp

---
+++ cs-helloworld/helloworld.cs
@@ -0,0 +1,14 @@
+using System;
+  
+namespace CS-Helloworld {
+    
+    class HelloWorld {
+          
+        static void Main(string[] args) {
+              
+            Console.WriteLine("Hello World!");
+              
+            Console.ReadKey();
+        }
+    }
+}
